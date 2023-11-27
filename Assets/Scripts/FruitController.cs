31393e25684d5f92c437261e5d6dd7cb1b1b5d64
IWWEENNI <99537408+IWWEENNI@users.noreply.github.com>
2023-11-27 22:34:56 +0100

    Sistema para consumir crutas y escalar personaje hechos

---
+++ Assets/Scripts/FruitController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitController : MonoBehaviour
+{
+    private Animator anim;
+    public FruitType fruitType;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void CollectFruit()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, 1);
+        anim.SetTrigger("Collect"); 
+    }
+}
+
