5d8a6591801b434ad8d63719a6e0ac29494e4a2e
Yixin <yixin875@gmail.com>
2023-12-03 16:50:35 +0100

    Bunny Now jumps

--- Assets/Scripts/BunnyMovement.cs
+++ Assets/Scripts/BunnyMovement.cs
@@ -4,57 +4,84 @@ using UnityEngine;
 using System;
 
 public class BunnyMovement : MonoBehaviour
-{ 
-    
-    private Rigidbody2D rb;
-    private float jumpForce = 100000000000000;
-    float elapsedTime = 0;
-    public float speed; 
+{
 
+    private float jumpForce = 5;
+    private float elapsedTime = 0;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private MyPhysics2D physics;
+
+    private bool _hasJumped = false;
     void Start()
     {
-         rb = GetComponent<Rigidbody2D>();
-        
+        rb = GetComponent<Rigidbody2D>();
+        physics = GetComponent<MyPhysics2D>();
+        speed = 0.5f;
     }
 
     void Update()
     {
-        intervalo();
-        _Movement();
-        
+        _Intervalo();
     }
-    private void intervalo() {
+    private void _Intervalo()
+    {
         elapsedTime += Time.deltaTime;
-        _Movement();
+
         if (elapsedTime >= 1.5f)
         {
             _ChangeDirecction();
+
             elapsedTime = 0;
-            _FlipHorizontally();
-            Jump();
         }
-         
-    } 
-    private void _ChangeDirecction() {
+        else if (elapsedTime >= 0.45f && elapsedTime <= 0.5f)
+        {
+            if (physics.isGround)
+                _Jump();
+        }
+        else
+        {
+            _Movement();
+        }
+
+    }
+    private void _ChangeDirecction()
+    {
         speed *= -1;
+        _FlipHorizontally();
     }
     private void _Movement()
     {
-         
-        rb.velocity = new Vector2(speed, 0);
+        if (physics.isGround)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
     }
-    
-    
-    private void Jump()
+
+
+    private void _Jump()
     {
         Debug.Log("Jumped");
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
+        if (!_hasJumped)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            _hasJumped = true;
+            StartCoroutine(_WaitXTimeBeforejumpingAgain());
+        }
     }
     private void _FlipHorizontally()
     {
         Vector2 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+    }
+
+    private IEnumerator _WaitXTimeBeforejumpingAgain()
+    {
+        yield return new WaitForSeconds(1.0f);
+        _hasJumped = false;
     }
 }
