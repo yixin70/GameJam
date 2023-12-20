abd4911ddd6b572df9269aef1092efa84c35be7b
Ouzhan Lin <ouzhan.lin@alumnos.upm.es>
2023-12-20 22:45:02 +0100

    UI menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,29 +10,20 @@ public class GameManager : Singleton<GameManager>
 {
     public GameObject player;
     public MapManager mapManager;
-    private AudioSource audioSource;
+
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         DontDestroyOnLoad(this.gameObject);
-        audioSource.Play();
     }
     public void StartGame()
     {
-        audioSource.Stop();
-        audioSource.Play();
         SceneManager.LoadScene(1);
     }
 
     public void Update()
     {
         // Si pulsamos escape abrir menu.
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            
-        }
     }
-   
 
     // Finaliza el juego.
     public void FinishGame()
